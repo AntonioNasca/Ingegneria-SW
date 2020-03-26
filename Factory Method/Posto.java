428c08513f73a37e4103682ac10e9681da89614f
Antonio <45246670+AntonioNasca@users.noreply.github.com>
2020-03-26 21:19:05 +0100

    Create Posto.java

---
+++ Factory Method/Posto.java
@@ -0,0 +1,10 @@
+
+/**Product Factory Method */
+//Posto è un product del Factory Method
+public interface Posto
+{
+    public String getPosizione();
+    public int getCosto();
+    public String getSettore();
+
+}
